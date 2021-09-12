dca9f60586067351a32b325dac917166f1ee42b4
Vítor <vrgermano159@gmail.com>
2021-09-12 17:56:54 -0300

    implementando imagens pagina principal

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,20 @@
+const chaveAPI = "c2948389"
+
+$.ajax({
+    method : 'GET',
+    url : `http://www.omdbapi.com/?apikey=${chaveAPI}&t=babe`,
+    success : (data) => {
+        console.log(data);
+        $("#capa").html(`
+        <img id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BYjg4ZjUzMzMtYzlmYi00YTcwLTlkOWUtYWFmY2RhNjliODQzXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg">
+        <img id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">
+        <img id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BOTM5N2ZmZTMtNjlmOS00YzlkLTk3YjEtNTU1ZmQ5OTdhODZhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">
+        <img id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg">
+        `);
+    }
+})
+
 $(document).ready(()=> {
-    const chaveAPI = "c2948389"
     $("#movieForm").submit((e)=>{
         e.preventDefault()
         let filme = $("#filme").val()
@@ -10,8 +25,9 @@ $(document).ready(()=> {
             url : url,
             success : (data) => {
                 console.log(data);
+                capa.style.display = "none";
                 retorno = `
-                <img src="${data.Poster}">
+                <img id="imgRetorno" src="${data.Poster}">
                 <h4>${data.Year}</h4>
                 <h4>${data.Country}</h4>
                 <h4>${data.Genre}</h4>
