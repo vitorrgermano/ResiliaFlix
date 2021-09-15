9c71a1fe7a895ea10ae9d48acce0e23908d26692
Vítor <vrgermano159@gmail.com>
2021-09-14 21:47:59 -0300

    colocacao das imagens na capa

--- scripts/search.js
+++ scripts/search.js
@@ -1,24 +1,5 @@
 const chaveAPI = "c2948389" //Chave API registro Vítor
 
-/*
-
-Comentário sobre modelo da página inicial.
-
-$.ajax({
-    method : 'GET',
-    url : `http://www.omdbapi.com/?apikey=${chaveAPI}&t=babe`,
-    success : (data) => {
-        console.log(data);
-        $("#capa").html(`
-        <img class="imgCapa" id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BYjg4ZjUzMzMtYzlmYi00YTcwLTlkOWUtYWFmY2RhNjliODQzXkEyXkFqcGdeQXVyNTUyMzE4Mzg@._V1_SX300.jpg">
-        <img class="imgCapa" id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">
-        <img class="imgCapa" id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BOTM5N2ZmZTMtNjlmOS00YzlkLTk3YjEtNTU1ZmQ5OTdhODZhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg">
-        <img class="imgCapa" id="imgRetorno" src="https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg">
-        `);
-    }
-})
-*/
-
 $(document).ready(()=> {
     $("#movieForm").submit((e)=>{
         e.preventDefault()
