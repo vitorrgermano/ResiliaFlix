c4c1dd99317d7b52e170e843d53dbd39823eca38
Vítor <vrgermano159@gmail.com>
2021-09-19 21:59:58 -0300

    página inicial atraves do API

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
 const chaveAPI = "c2948389" //Chave API registro Vítor
-var filme = "harry";
+var filme = ["love","tomorrow","home","harry"];
 
-$.ajax({
-    method : 'GET',
-    url : `http://www.omdbapi.com/?apikey=${chaveAPI}&s=${filme}`,
-    success : (data) => {
-        console.log(data);
-        $("#capa").html(`
-        <img class="imgCapa" id="imgRetorno" src="${data.Poster}">
-        `);
-    }
-})
+for(let j=0; j<filme.length; j++){
+try {
+    $.ajax({
+        method: 'GET',
+        url: `http://www.omdbapi.com/?apikey=${chaveAPI}&s=${filme[j]}`,
+        success: (data) => {
+            if (data.Response == "False") throw new Error(data.Error);
+            console.log(data.Search);
+            for (let i = 0; i < data.Search.length; i++) {
+                $("#retorno").append(`
+                <div class="filmeInfo">
+                <h6>Título: ${data.Search[i].Title}</h6>
+                <h6>Ano do filme: ${data.Search[i].Year}</h6>
+                <img class="imgCapa" id="imgRetorno" src="${data.Search[i].Poster}">
+                </div>
+                `);
+            }
+        }
+    })
+} catch (erro) {
+    console.log(erro)
+}
+}
