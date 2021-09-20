a1840f0d82388ddc898db5b6bc3398a88aec4a59
Vítor <vrgermano159@gmail.com>
2021-09-19 21:30:21 -0300

    Inicio uso API para capas main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 const chaveAPI = "c2948389" //Chave API registro Vítor
+var filme = "harry";
 
 $.ajax({
     method : 'GET',
-    url : `http://www.omdbapi.com/?apikey=${chaveAPI}&t=babe`,
+    url : `http://www.omdbapi.com/?apikey=${chaveAPI}&s=${filme}`,
     success : (data) => {
         console.log(data);
         $("#capa").html(`
